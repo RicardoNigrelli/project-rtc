773f9f52a519a27bc7d942a3b5526f629ff030e0
Ricardo Nigrelli <ricanigrelli996@gmail.com>
2024-06-15 17:25:55 -0300

    Relación con usuario logueado v1

--- server/index.js
+++ server/index.js
@@ -24,7 +24,8 @@ const db = createClient({
 
 await db.execute(`CREATE TABLE IF NOT EXISTS messages (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    content TEXT)`);
+    content TEXT,
+    user TEXT)`);
 
 await db.execute(`CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -46,7 +47,7 @@ server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
   console.log("User connected");
 
   socket.on("disconnect", () => {
@@ -55,16 +56,17 @@ io.on("connection", (socket) => {
 
   socket.on("chat message", async (msg) => {
     let result;
+    const username = socket.handshake.auth.username ?? "anonymous";
     try {
       result = await db.execute({
-        sql: `INSERT INTO messages (content) VALUES (:msg)`,
-        args: { msg },
+        sql: `INSERT INTO messages (content, user) VALUES (:msg, :username)`,
+        args: { msg, username},
       });
     } catch (e) {
       console.error(e);
       return;
     }
-    io.emit("chat message", msg, result.lastInsertRowid.toString());
+    io.emit("chat message", msg, result.lastInsertRowid.toString(), username);
   });
 
   socket.on("register", async ({ username, password }) => {
@@ -129,4 +131,21 @@ io.on("connection", (socket) => {
       socket.emit("login", { success: false, message: "Login failed" });
     }
   });
+
+  if (!socket.recovered) {
+    try {
+      const results = await db.execute({
+        sql: "SELECT id, content, user FROM messages WHERE id > ?",
+        args: [socket.handshake.auth.serverOffset ?? 0]
+      })
+
+      results.rows.forEach(row => {
+        socket.emit("chat message", row.content, row.id.toString(), row.username);
+      });
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
 });
